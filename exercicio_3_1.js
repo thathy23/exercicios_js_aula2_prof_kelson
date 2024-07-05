deb2fccd590af6b4253779dee821af6b5764bcd5
thathy23 <thathy23@gmail.com>
2024-07-05 16:37:00 -0300

    exercicio3_1

--- exercicio_3_1.js
+++ exercicio_3_1.js
@@ -8,3 +8,11 @@ O programa deve:
 ○ Utilize a sintaxe temperaturas[Math.floor(Math.random() * temperaturas.length)] para selecionar um item 
 aleatório do array. */
 
+const temperaturas = [20,21,22,23,];
+var fahrenheit = (temperaturas * 9/5) + 32;
+
+console.log (`As temperatura são: ${temperaturas[0]}°C, ${temperaturas[1]}°C, ${temperaturas[2]}°C, ${temperaturas[3]}°C`)
+
+console.log (`A temperatura sorteada foi: ${temperaturas[Math.floor(Math.random() * temperaturas.length)]}°C`)
+
+console.log (`A temperatura sorteada corresponde à ${(temperaturas[Math.floor(Math.random() * temperaturas.length)]) * 9/5 + 32}°F`)
